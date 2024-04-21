ed4bd946337c6d91dbd2f5b442da87c8e61fd87a
Navodith09 <55744332+Navodith09@users.noreply.github.com>
2024-04-21 18:33:28 +0000

    trials

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+const accountId = 14473
+let accountEmail = "asdf.com"
+let accountType = "saving"
+let accountState
+
+// console.log(accountId)
+
+accountEmail = "mohana.anc"
+accountType = "funny"
+
+console.table([accountId, accountEmail, accountType, accountState])
