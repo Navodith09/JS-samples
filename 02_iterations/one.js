f3b18f156b15ebc91b8eb590f2265582aef7cef6
Navodith Mondal <navodith.saptarshi@gmail.com>
2024-07-29 09:48:37 +0000

    for loop, while loop, do-while loop

---
+++ 02_iterations/one.js
@@ -0,0 +1,45 @@
+// for loop
+// if condition inside for loop
+// break statement
+
+// let n = 12
+// for(let i=0; i<n; i++) {
+//     if (i>9) {
+//         console.log(`and this goes on ...`);
+//         break;
+//     }
+//     console.log(`Hello World #${i+1}`);
+// }
+
+
+
+// Iterating within an Array using for loop
+// continue statement
+
+let heroes = ["Iron-man", "Spider-man", "Thor", "Thanos", "Black Panther"];
+
+// for(let i=0; i<heroes.length; i++) {
+//     if (heroes[i] === "Thanos") {
+//         console.log(`${heroes[i]} is a villain`);
+//         continue;
+//     }
+//     console.log(`I love ${heroes[i]}`);
+
+// }
+
+
+
+// while loop
+
+let n=0
+// while (n<heroes.length) {
+//     console.log(`Hello, ${heroes[n]}`);
+//     n = n+1;
+// }
+
+//do-while loop
+
+// do {
+//     console.log(`Hi, ${heroes[n]}`);
+//     n = n+1;
+// } while(n>9);
