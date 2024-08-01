16b0ffc0540da2c2f2255aa3569f8c234c881aeb
Navodith Mondal <navodith.saptarshi@gmail.com>
2024-08-01 07:30:17 +0000

    DOM Operations

---
+++ 03_dom/02_script_1.js
@@ -0,0 +1,40 @@
+// DOM = Document Object Model
+
+
+
+// selecting items using 'getElementByClassName()' and 'getElementByID()' -->
+
+// const head1 = document.getElementsByClassName('main-heading');
+
+// const para = document.getElementById('para1');
+
+
+
+// selecting items using 'querySelector()' -->
+
+// const ulist = document.querySelector('.lists');
+
+// const oneItem = document.querySelector('#item');
+
+// const li = document.querySelectorAll('item');
+
+
+
+// selecting or manipulating inner contents of html tags -->
+
+// .innerText --> gives the inner texts
+
+// .innerHTML --> gives the inner texts as well as the HTML contents in it
+
+
+
+// .append() --> adds the functionality at the end
+
+// .remove() --> deletes the functionality
+
+
+
+// We can also change the stylings (CSS works)
+
+// head1.style.backgroundColor = 'green';
+// head1.style.color = 'red';
